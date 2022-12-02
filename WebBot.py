a9e64f82897c30b0bfef34d0333bc0eda8cdd2ca
Wesleyhdias <firstweshenri@gmail.com>
2022-12-01 21:02:10 -0300

    chegando até a aba notas do site

--- WebBot.py
+++ WebBot.py
@@ -12,7 +12,8 @@ web.get("https://feitep.jacad.com.br/academico/aluno-v2/login")
 web.find_element(By.ID, 'login').send_keys('4245')
 web.find_element(By.ID, 'senha').send_keys('09052004')
 web.find_element(By.ID, 'btn-login').click()
-# ta dando errado ainda
-
+web.find_element(By.XPATH, '//*[@id="layout-sidenav"]/ul/li[2]/a').click()
+sleep(0.1)
+web.find_element(By.XPATH, '//*[@id="layout-sidenav"]/ul/li[2]/ul/li[1]').click()
 
 sleep(5)
