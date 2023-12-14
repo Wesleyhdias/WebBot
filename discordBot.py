30908d2cb59cabec70f82bedceba1ad3993f4f99
Wesleyhdias <wesleyhdmelo@gmail.com>
2023-12-14 01:06:56 -0300

    terminando embed e outras coisas

--- discordBot.py
+++ discordBot.py
@@ -1,10 +1,12 @@
 import utils
 import discord
+from decouple import config
 from discord.ext import commands, tasks
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix = "?", intents = intents)
+escola = 'https://feitep.jacad.com.br/academico/aluno-v2/login'
 
 @bot.event
 async def on_ready():
@@ -15,18 +17,20 @@ async def on_ready():
 async def on_message(message):
     if message.author == bot.user:
         return
-
+    
     await bot.process_commands(message)
 
 
-@bot.command(name = 'notas') 
-async def get_grade(ctx, site, login, password):
-    
-    # ainda tem coisas para arrumar aqui, muitas coisas
+# comando para ver as notas
+@bot.command(name = 'notas', help='envia para o seu privado uma tabela com suas notas atualizadas direto do site de sua escola ou faculdade, os argumentos desse comando devem estar em ordem e separados por um espaço') 
+async def get_grade(ctx, login, password):
     try:
-        notas = utils.get_grades(site, login, password, toStr=False)
+        if str(ctx.channel) != "Direct Message with Unknown User":
+            await ctx.message.delete()
+        notas = utils.get_grades(escola, login, password, toStr=False)
         utils.drawText(notas)
         
+        # cria embed para enviar
         embed = discord.Embed(
             title = "Tabela de notas",
             description= "Podem haver erros ou mudanças nas notas",
@@ -35,21 +39,22 @@ async def get_grade(ctx, site, login, password):
         embed.set_image(url='attachment://image.png')
         
         await ctx.author.send(f'aqui estão suas notas até o momento:\n\n')
-        await ctx.send(file=file, embed=embed)
-                           
+        await ctx.author.send(file=file, embed=embed)
+        
+    # caso não tenha permição para mensagens diretas
     except discord.errors.Forbidden:
-        if notas == False:
-            await ctx.send('não encontrei nenhuma nota')
-            await ctx.author.send('mas não se preocupe, os professores podem simplesmente não ter postado ainda, pesquise outra vez mais tarte, ou fale diretamente com algum professor')
-        else:
-            await ctx.send('Não posso enviar menssagens no seu privrado!\ncaso queira receber habilite a opção para receber mensagens de qualquer pessoa.')
-    
+        await ctx.send('Não posso enviar menssagens no seu privrado!\ncaso queira receber habilite a opção para receber mensagens de qualquer pessoa.')
+    # para erro desconhecido
+    except Exception as erro:
+        print(f"erro novo\n\n{erro}\n\n")
 
-# ainda não tenho o porque usar algo assim, mas tá aqui no caso de algum dia precisar
+# verificar atualizações no site(falta fazer)
 @tasks.loop(seconds = 5) 
 async def remenber():
     channel = bot.get_channel(1055487703472406538)
     
-    await channel.send('eu ainda estou aqui!')
+    await channel.send('verificando')
 
-bot.run('token')
+
+TOKEN = config('TOKEN')
+bot.run(TOKEN)
